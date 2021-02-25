292e19d4e76d9079d2e8bc6250ffcc342e13d1cd
Mario Lora Rocha <mloraroc@ucsd.edu>
2021-02-24 22:31:04 -0800

    thirdpartyAPIlab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = '87Tt6udP9ochzDS53wS3YWaa0S954fI0';
+
+// 'map' refers to a <div> element with the ID map
+var map = L.mapquest.map('map', {
+  center: [32.88, -117.236],
+  layers: L.mapquest.tileLayer('map'),
+  zoom: 12,
+  zoomControl: false
+});
+
+	L.marker([32.88, -117.236]).addTo(map);
 }
